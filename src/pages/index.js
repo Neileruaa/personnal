bf0ed09b143f6fbb99f48dc17eef71ac2059f4cc
aurelien <aureliendrey@gmail.com>
2020-01-29 18:41:44 +0100

    change in layout and better footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,6 @@ import SEO from "../components/seo"
 const IndexPage = () => (
     <Layout>
         <SEO title="Home"/>
-        <h1 className="title">Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>
-            <Image/>
-        </div>
-        <Link to="/page-2/">Go to page 2</Link>
     </Layout>
 );
 
